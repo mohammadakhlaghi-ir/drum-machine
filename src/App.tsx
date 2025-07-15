554ccbb3527efd1d4f994d4a2fc976ff82f9e95c
wolfsmad <michaelsevii17@gmail.com>
2025-07-15 20:02:47 +0330

    version 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import DrumPad from './components/DrumPad';
 
 const sounds = [
-  { key: 'Q', label: 'Heater-1', src: '/sounds/Heater-1.mp3' },
-  { key: 'W', label: 'Heater-2', src: '/sounds/Heater-2.mp3' },
-  { key: 'E', label: 'Heater-3', src: '/sounds/Heater-3.mp3' },
-  { key: 'A', label: 'Heater-4', src: '/sounds/Heater-4_1.mp3' },
-  { key: 'S', label: 'Clap', src: '/sounds/Heater-6.mp3' },
-  { key: 'D', label: 'Open-HH', src: '/sounds/Dsc_Oh.mp3' },
-  { key: 'Z', label: "Kick-n'-Hat", src: '/sounds/Kick_n_Hat.mp3' },
-  { key: 'X', label: 'Kick', src: '/sounds/RP4_KICK_1.mp3' },
-  { key: 'C', label: 'Closed-HH', src: '/sounds/Cev_H2.mp3' },
+  { key: 'Q', label: 'Heater-1', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+  { key: 'W', label: 'Heater-2', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+  { key: 'E', label: 'Heater-3', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+  { key: 'A', label: 'Heater-4', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+  { key: 'S', label: 'Clap', src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+  { key: 'D', label: 'Open-HH', src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+  { key: 'Z', label: "Kick-n'-Hat", src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+  { key: 'X', label: 'Kick', src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+  { key: 'C', label: 'Closed-HH', src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' },
 ];
 
 const App: React.FC = () => {
